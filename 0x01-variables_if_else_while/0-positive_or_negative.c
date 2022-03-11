1154b553563f0369a2bdcd6d60e8b1b3414272b5
LabGich <labradorgichohi@gmail.com>
2022-03-11 03:04:47 -0800

    Looping loops

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
@@ -12,7 +13,7 @@ if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-else if (n == 0);
+else if (n == 0)
 {
 printf("%d\ is zero\n", n);
 }
